600bd6ec0530e7d6f301028ca5986cbccc1ab9b7
Danny Hwang <dannyhwangca@gmail.com>
2020-04-07 13:48:44 -0700

    added fontawesome and base main structure to app.js

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <div className="master-outer-div">
+        <div className="master-inner-div">
+          <div className="left-outer-container">
+            <div className="left-inner-container">
+              <FontAwesomeIcon icon={faCoffee} />
+            </div>
+          </div>
+          <div className="right-outer-container">
+            <div className="right-inner-container">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;

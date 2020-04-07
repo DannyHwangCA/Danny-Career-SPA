fdd1854a82a63b0a7ade1df16867157c6fff9372
Danny Hwang <dannyhwangca@gmail.com>
2020-04-07 14:16:55 -0700

    added components folder, css folder, removed unncessary files, cleaned index html

--- spa/src/components/App.js
+++ spa/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '../css/App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 

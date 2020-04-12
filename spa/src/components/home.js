741561dfef0e10142c6e76eebb2364215ea08da2
Danny Hwang <dannyhwangca@gmail.com>
2020-04-11 20:43:59 -0700

    added css to projects, cleaned  projects json

--- spa/src/components/home.js
+++ spa/src/components/home.js
@@ -6,13 +6,13 @@ class Home extends React.Component {
             <div className="home-outer-container">
                 <div className="home-intro">
                     <h3>Danny Shyh Ru Hwang. Web developer. Frontend engineer.</h3>
-                    <p>I am a web developer and frontend engineer with a passion for coding!</p>
-                    <p>This site is used to display some of my projects, resume, and contact information.</p>
+                    <p>Hello! Welcome to my responsive React Information SPA.</p>
+                    <p>This SPA is used to display some of my projects, resume, and contact information.</p>
                 
-                    <h3>About this site.</h3>
+                    <h3>About this SPA</h3>
                     <p>This responsive SPA is build with React.JS and served over Amazon web service.</p>
                     <p>Each nav button renders a separate Component within this main container and uses multiple states and props to affect changes across the page.</p>
-                    <p>Click on the "SPA" in the nav bar for more information.</p>
+                    <p>Click on the "SPA" in the nav bar for more information about this SPA.</p>
                 </div>
 
                 <div className="outer-library-container">
--- spa/src/components/projects.js
+++ spa/src/components/projects.js
@@ -4,24 +4,26 @@ class Projects extends React.Component {
     render() {
         const projectItems = this.props.projects.map((item, index) => (
             <div className="project-item" key={index}>
-                <div className="project-type">
+                <div className="project-type-container">
                     <div className={item.type}>
-                        <h3>
-                            {item.type}
+                        <h3 className="project-type">
+                            {item.type} Project
                         </h3>
                     </div>
                 </div>
-                <div className="project-title">
-                <h2><a href={item.link}>{item.title}</a></h2>
-                </div>
                 <div className="project-image">
-                <a href={item.link}><img src={item.image}/></a>
+                    <a href={item.link} target="_blank">
+                        <img src={item.image}/>
+                    </a>
                 </div>
-                <div className="project-date">
-                    <h3>{item.date}</h3>
+                <div className="project-title">
+                    <h2><a href={item.link} target="_blank">{item.title}</a></h2>
+                    <h3>{item.subtitle}</h3>
                 </div>
                 <div className="project-description">
-                {item.description}
+                    <p>
+                        {item.description}
+                    </p> 
                 </div>
             </div>
         ));
@@ -31,11 +33,12 @@ class Projects extends React.Component {
                     <h2>
                         Projects and Pens
                     </h2>
-                    <p>Below is a list of my projects and codepens.
-                        <br />
-                        This is meant as an example archive of my code. 
-                        <br />
-                        Please feel free to look through them! 
+                    <p>
+                        This Component archives my Projects that highlight different concepts in Frontend Engineering and Web Development. Please feel free to look through them!
+                        <br/><br/>
+                        <b>Codepen:</b><a href="https://codepen.io/dannyhwangca" target="_blank">https://codepen.io/dannyhwangca</a>
+                        <br/><br/>
+                        <b>Github:</b><a href="https://github.com/DannyHwangCA" target="_blank">https://github.com/DannyHwangCA</a> 
                     </p>
                 </div>
                 <div className="project-list-container">

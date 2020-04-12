d8849e1b2875bb0f1e3cf33a39065af46f0d3739
Danny Hwang <dannyhwangca@gmail.com>
2020-04-11 23:22:16 -0700

    fixed project images, made project space wider

--- spa/src/components/home.js
+++ spa/src/components/home.js
@@ -8,9 +8,7 @@ class Home extends React.Component {
                     <h3>Danny Shyh Ru Hwang. Web developer. Frontend engineer.</h3>
                     <p>Hello! Welcome to my responsive React Information SPA.</p>
                     <p>This SPA is used to display some of my projects, resume, and contact information.</p>
-                
-                    <h3>About this SPA</h3>
-                    <p>This responsive SPA is build with React.JS and served over Amazon web service.</p>
+                    <p>I built this responsive SPA is build with React.JS and served over Amazon web service.</p>
                     <p>Each nav button renders a separate Component within this main container and uses multiple states and props to affect changes across the page.</p>
                     <p>Click on the "SPA" in the nav bar for more information about this SPA.</p>
                 </div>

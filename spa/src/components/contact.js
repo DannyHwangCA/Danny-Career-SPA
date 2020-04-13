dbdf70c31a3f250de1ba3321e14d1e2faeae098a
Danny Hwang <dannyhwangca@gmail.com>
2020-04-12 19:22:05 -0700

    fixed css to contact page

--- spa/src/components/contact.js
+++ spa/src/components/contact.js
@@ -9,19 +9,19 @@ class Contact extends React.Component {
         return ( 
             <div className="contact-container">
                 <div className="left-contact-container">
-                    <div className="single-nav-icon project-icon">
+                    <div className="left-contact-container-icon project-icon">
                         <FontAwesomeIcon icon={faEnvelopeSquare} />
                         <h3>Email</h3>
                     </div>
-                    <div className="single-nav-icon project-icon">
+                    <div className="left-contact-container-icon project-icon">
                         <FontAwesomeIcon icon={faLinkedin} /><i class="fab fa-linkedin"></i>
                         <h3>Linkedin</h3>
                     </div>
-                    <div className="single-nav-icon project-icon">
+                    <div className="left-contact-container-icon project-icon bottom-left-icon-adjust">
                         <FontAwesomeIcon icon={faGithubSquare} />
                         <h3>Github</h3>
                     </div>
-                    <div className="single-nav-icon project-icon">
+                    <div className="left-contact-container-icon project-icon bottom-left-icon-adjust">
                         <FontAwesomeIcon icon={faCodepen} />
                         <h3>Codepen</h3>
                     </div>
@@ -36,16 +36,20 @@ class Contact extends React.Component {
                         <b>Phone: </b><a href="tel:6262445621" target="_blank">(626) 244-5621</a>
                         <br/>
                         <b>Location: </b>Greater Los Angeles Area - Eastvale, CA 92880
-                        <br/><br/>
-                        <b>Resume (updated April 2020): </b><a href={resume} target="_blank" download>Download Here</a>
-                        <br/><br/>
-                        <b>Links</b>
-                        <br/>
+                    </p>
+                    <br/>
+                    <h2>
+                        Links
+                    </h2>
+                    <p>
                         <b>Linkedin: </b><a href="https://www.linkedin.com/in/dannyhwangca/" target="_blank">https://www.linkedin.com/in/dannyhwangca/</a> 
                         <br/>
                         <b>Codepen: </b><a href="https://codepen.io/dannyhwangca" target="_blank">https://codepen.io/dannyhwangca</a>
                         <br/>
                         <b>Github: </b><a href="https://github.com/DannyHwangCA" target="_blank">https://github.com/DannyHwangCA</a> 
+                    </p>
+                    <p>
+                        <b>Resume (updated April 2020): </b><a href={resume} target="_blank" download>Download Here</a>
                     </p>
                 </div>
                 

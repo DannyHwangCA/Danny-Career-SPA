34ccb6feb2064c1d9c70545ad5086790e0d4ba5f
Danny Hwang <dannyhwangca@gmail.com>
2020-05-05 23:38:06 -0700

    added blog page

---
+++ spa/src/components/Blog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Blog extends React.Component {
+    render() {
+        return(
+            <div>
+                Blog Info Goes Here.
+            </div>
+        )
+    }
+}
+
+export default Blog;

5673d8d8376625c60f0e040766731756af117c88
Danny Hwang <dannyhwangca@gmail.com>
2020-05-06 23:05:23 -0700

    added base structure

--- spa/src/components/Single.js
+++ spa/src/components/Single.js
@@ -3,8 +3,15 @@ import React from 'react';
 class Single extends React.Component {
     render() {
         return(
-            <div>
-                Single Stylized Page
+            <div className="single-outer-container">
+                <div className="single-inner-container">
+                    <div className="single-nav-container">
+
+                    </div>
+                    <div className="single-content-container">
+
+                    </div>
+                </div>
             </div>
         )
     }
